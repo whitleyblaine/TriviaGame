9ef6f5e2bcd1114381025d3d85875386a2e52f4d
Whitley Blaine <whitleyblaine@gmail.com>
2016-05-30 17:58:18 -0400

    Serious game progress

---
+++ assets/javascript/app.js
@@ -0,0 +1,57 @@
+var question = 0;
+
+$(document).ready(function() {
+    console.log( "document loaded" );
+});
+
+$( window ).load(function() {
+    console.log( "window loaded" );
+});
+
+// $(document).ready(function() {
+//   $('#start').click(function() {
+//     question++;
+//     $(this).addClass('hide');
+//     $('#question').removeClass("hide");
+//     $('#question').html(
+//       '<p>Question 1!</p>' +
+//       '<p>Who said THIS?!</p>' +
+//       '<p>"Part of the beauty of me is that I\'m very rich."</p>' +
+//       "<div id='answer1' class='correct choice'><p>Donald Trump</p></div>" +
+//       "<div id='answer2' class='incorrect choice'><p>Scrooge McDuck</p></div>" +
+//       "<div id='answer3' class='incorrect choice'><p>Mr. Krabs</p></div>" +
+//       "<div id='answer4' class='incorrect choice'><p>Charles Montgomery Plantagenet Schicklgruber Burns</p></div>"
+//     );
+//   });
+
+
+//   $('.correct').click(function() {
+//     alert("Correct!");
+//     $('#question').html(
+//       '<p>Question 2!</p>' +
+//       '<p>Who said THIS?!</p>' +
+//       '<p>“I have a great relationship with the blacks. I’ve always had a great relationship with the blacks.”</p>' +
+//       "<div id='answer1' class='incorrect choice'><p>Martin Luther King</p></div>" +
+//       "<div id='answer2' class='correct choice'><p>Donald Trump</p></div>" +
+//       "<div id='answer3' class='incorrect choice'><p>Jesse Jackson</p></div>" +
+//       "<div id='answer4' class='incorrect choice'><p>George Bush</p></div>"
+//     );
+//     question++;
+//     alert(question);
+//   });
+
+  // // $('.correct').click(function() {
+  // //   alert("Correct!");
+  // //   $('#question').html(
+  // //     '<p>Question 3!</p>' +
+  // //     '<p>Who said THIS?!</p>' +
+  // //     '<p>“All of the women on The Apprentice flirted with me — consciously or unconsciously. That’s to be expected.”</p>' +
+  // //     "<div id='answer1' class='incorrect choice'><p>Martin Luther King</p></div>" +
+  // //     "<div id='answer2' class='correct choice'><p>Donald Trump</p></div>" +
+  // //     "<div id='answer3' class='incorrect choice'><p>Jesse Jackson</p></div>" +
+  // //     "<div id='answer4' class='incorrect choice'><p>George Bush</p></div>"
+  // //   );
+  // //   question++;
+  // // });
+// })
+
