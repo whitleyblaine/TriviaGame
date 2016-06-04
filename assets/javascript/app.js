9b2b49b3c9e6979b54696c0ad85690eb5207c76b
Whitley Blaine <whitleyblaine@gmail.com>
2016-06-04 15:56:48 -0400

    New gifs for each question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,11 @@ var timeLeft = 15;
 
 var intervalId;
 
+var imageURL1 = "assets/images/blast_off.gif";
+var imageURL2 = "assets/images/funny_face.gif";
+var imageURL3 = "assets/images/funny_face_2.gif";
+var imageURL4 = "assets/images/problem.gif";
+
 var startTimer = function () {
   intervalId = setInterval(countdown, 1000);
 
@@ -100,16 +105,23 @@ $('#start').on('click', function () {
 $('.incorrect').on('click', function () {
   wrong++;
   clearInterval(intervalId);
-  $('#funnyText2').removeClass('hide');
   $('.question').addClass('hide');
   $('.wrongAnswerPage').removeClass('hide');
   if (question == 1) {
+    $('#funnyGif').attr('src', imageURL1);
+    $('#funnyText2').removeClass('hide');
     var timeoutId = window.setTimeout(showQ2, 5 * 1000)
   } else if (question == 2) {
+    $('#funnyGif').attr('src', imageURL2);
+    $('#funnyText2').removeClass('hide');
     var timeoutId = window.setTimeout(showQ3, 5 * 1000)
   } else if (question == 3) {
+    $('#funnyGif').attr('src', imageURL3);
+    $('#funnyText2').removeClass('hide');
     var timeoutId = window.setTimeout(showQ4, 5 * 1000)
   } else if (question == 4) {
+    $('#funnyGif').attr('src', imageURL4);
+    $('#funnyText2').removeClass('hide');
     var timeoutId = window.setTimeout(showResults, 5 * 1000)
   };
 });
@@ -117,16 +129,25 @@ $('.incorrect').on('click', function () {
 $('.correct').on('click', function () {
   correct++;
   clearInterval(intervalId);
+  $('#funnyGif').attr('src', imageURL2);
   $('#funnyText2').removeClass('hide');
   $('.question').addClass('hide');
   $('.rightAnswerPage').removeClass('hide');
   if (question == 1) {
+    $('#funnyGif').attr('src', imageURL1);
+    $('#funnyText2').removeClass('hide');
     var timeoutId = window.setTimeout(showQ2, 5 * 1000)
   } else if (question == 2) {
+    $('#funnyGif').attr('src', imageURL2);
+    $('#funnyText2').removeClass('hide');
     var timeoutId = window.setTimeout(showQ3, 5 * 1000)
   } else if (question == 3) {
+    $('#funnyGif').attr('src', imageURL3);
+    $('#funnyText2').removeClass('hide');
     var timeoutId = window.setTimeout(showQ4, 5 * 1000)
   } else if (question == 4) {
+    $('#funnyGif').attr('src', imageURL4);
+    $('#funnyText2').removeClass('hide');
     var timeoutId = window.setTimeout(showResults, 5 * 1000)
   };
 });
